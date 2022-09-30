44b83790229657f25b16f80984ad9eb7b0828179
aisav <aisavikin@gmail.com>
2022-09-30 13:24:39 +0300

    Изменение структуры

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
-from openpyxl import load_workbook
-from pathlib import Path
+from openpyxl import load_workbook, styles
 from datetime import datetime
 from calendar import Calendar
 from conf import *
+from pathlib import Path
+import shutil
 
 MONTH_DICT = {
     'Сентябрь': (9, YEAR[0]), 'Октябрь': (10, YEAR[0]), 'Ноябрь': (11, YEAR[0]), 'Декабрь': (12, YEAR[0]),
@@ -28,6 +29,92 @@ def get_kids(path):
     names = ws['B16:B38']
     return [names[i][0].value for i in range(23) if names[i][0].value]
 
+
+# region NEW SHEET
+def create_new_sheet(file_name, base, month):
+    path = copy_sheet(file_name, base, month)
+    if not path:
+        return
+    group = path.name.split('_')[0]
+    clear_absent(path)
+    clear_note(path)
+    write_service_information(path, month, group)
+    colorize_weekend(path, month)
+    return path
+
+
+def copy_sheet(file_name, base, month):
+    if base == 'Новая группа':
+        path_template = Path(Path.cwd(), 'Template.xlsx')
+        new_path = Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
+        shutil.copyfile(path_template, new_path)
+        return Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
+    file_name = base.split('_')[0]
+    new_path = Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
+    try:
+        shutil.copyfile(f'Ведомости/{base}', new_path)
+    except shutil.SameFileError:
+        return
+    return Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
+
+
+def clear_note(path):
+    wb = load_workbook(path)
+    ws = wb['Заметки']
+    cells = 'A1:A20'
+    for i in ws[cells]:
+        i[0].value = ''
+    cells = 'B1:B20'
+    for i in ws[cells]:
+        i[0].value = ''
+
+    wb.save(path)
+
+
+def clear_absent(path):
+    # Очищает ячейки от "н, б"
+    # 16 - 39 диапазон строк, 5 - 39 диапазон столбцов
+    work_book = load_workbook(path)
+    ws = work_book['Посещаемость']
+    ws['C7'].value = ''
+    for row in range(16, 39):
+        for col in range(5, 39):
+            ws.cell(row=row, column=col).value = ''
+            ws.cell(row=row, column=col).fill = styles.PatternFill()
+
+    work_book.save(path)
+
+
+def colorize_weekend(path, month):
+    work_book = load_workbook(path)
+    ws = work_book['Посещаемость']
+    weekends = [day[0] for day in Calendar().itermonthdays2(MONTH_DICT[month][1], MONTH_DICT[month][0]) if
+                day[0] != 0 and day[1] in (5, 6)]
+    for row in range(16, 39):
+        for col in weekends:
+            ws.cell(row=row, column=col + 4).fill = styles.PatternFill(start_color='5E5E5E', fill_type='solid')
+
+    work_book.save(path)
+
+
+def write_service_information(path, month, group):
+    work_book = load_workbook(path)
+    ws = work_book['Посещаемость']
+    ws['N3'].value = month
+    ws['AA42'].value = month
+    ws['C5'].value = group
+    ws['V3'].value = MONTH_DICT[month][1]
+    ws['AG42'].value = MONTH_DICT[month][1]
+    ws = work_book['Заметки']
+    work_days = get_work_days(month)
+    row = 1
+    for day in work_days:
+        ws.cell(row=row, column=1).value = day
+        row += 1
+    work_book.save(path)
+
+
+# endregion
 
 if __name__ == '__main__':
     pass
--- windows/add_new_sheet.py
+++ windows/add_new_sheet.py
@@ -1,10 +1,6 @@
 import PySimpleGUI as sg
-import shutil
-from utils import MONTH_DICT, date, get_work_days
-from openpyxl import load_workbook, styles
+from utils import MONTH_DICT, date, create_new_sheet
 from conf import *
-from calendar import Calendar
-from pathlib import Path
 
 
 def add_new_sheet(file_name: str, list_group: list):
@@ -62,92 +58,6 @@ def add_new_sheet(file_name: str, list_group: list):
 
     window.close()
 
-
-# region NEW SHEET
-def create_new_sheet(file_name, base, month):
-    path = copy_sheet(file_name, base, month)
-    if not path:
-        return
-    group = file_name.split('_')[0]
-    clear_absent(path)
-    clear_note(path)
-    write_service_information(path, month, group)
-    colorize_weekend(path, month)
-    return path
-
-
-def copy_sheet(file_name, base, month):
-    if base == 'Новая группа':
-        path_template = Path(Path.cwd(), 'Template.xlsx')
-        new_path = Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
-        shutil.copyfile(path_template, new_path)
-        return Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
-    file_name = base.split('_')[0]
-    new_path = Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
-    try:
-        shutil.copyfile(f'Ведомости/{base}', new_path)
-    except shutil.SameFileError:
-        return
-    return Path(Path.cwd(), 'Ведомости', f'{file_name}_{month}.xlsx')
-
-
-def clear_note(path):
-    wb = load_workbook(path)
-    ws = wb['Заметки']
-    cells = 'A1:A20'
-    for i in ws[cells]:
-        i[0].value = ''
-    cells = 'B1:B20'
-    for i in ws[cells]:
-        i[0].value = ''
-
-    wb.save(path)
-
-
-def clear_absent(path):
-    # Очищает ячейки от "н, б"
-    # 16 - 39 диапазон строк, 5 - 39 диапазон столбцов
-    work_book = load_workbook(path)
-    ws = work_book['Посещаемость']
-    ws['C7'].value = ''
-    for row in range(16, 39):
-        for col in range(5, 39):
-            ws.cell(row=row, column=col).value = ''
-            ws.cell(row=row, column=col).fill = styles.PatternFill()
-
-    work_book.save(path)
-
-
-def colorize_weekend(path, month):
-    work_book = load_workbook(path)
-    ws = work_book['Посещаемость']
-    weekends = [day[0] for day in Calendar().itermonthdays2(MONTH_DICT[month][1], MONTH_DICT[month][0]) if
-                day[0] != 0 and day[1] in (5, 6)]
-    for row in range(16, 39):
-        for col in weekends:
-            ws.cell(row=row, column=col + 4).fill = styles.PatternFill(start_color='5E5E5E', fill_type='solid')
-
-    work_book.save(path)
-
-
-def write_service_information(path, month, group):
-    work_book = load_workbook(path)
-    ws = work_book['Посещаемость']
-    ws['N3'].value = month
-    ws['AA42'].value = month
-    ws['C5'].value = group
-    ws['V3'].value = MONTH_DICT[month][1]
-    ws['AG42'].value = MONTH_DICT[month][1]
-    ws = work_book['Заметки']
-    work_days = get_work_days(month)
-    row = 1
-    for day in work_days:
-        ws.cell(row=row, column=1).value = day
-        row += 1
-    work_book.save(path)
-
-
-# endregion
 
 if __name__ == '__main__':
     pass
